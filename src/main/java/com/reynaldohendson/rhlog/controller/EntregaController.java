17206b15cd0ade9e644288572e7bf3c73158bd91
Reynaldo Hendson <80369346+reynaldo86@users.noreply.github.com>
2023-01-29 22:02:18 -0300

    feat: Adiciona nova classe de configuração.

--- src/main/java/com/reynaldohendson/rhlog/controller/EntregaController.java
+++ src/main/java/com/reynaldohendson/rhlog/controller/EntregaController.java
@@ -7,6 +7,7 @@ import com.reynaldohendson.rhlog.repository.EntregaRepository;
 import com.reynaldohendson.rhlog.service.SolicitacaoEntregaService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +20,7 @@ import java.util.List;
 public class EntregaController {
     private SolicitacaoEntregaService solicitacaoEntregaService;
     private EntregaRepository entregaRepository;
+    private ModelMapper modelMapper;
     @PostMapping
     public ResponseEntity<Entrega> solicitar(@RequestBody @Valid Entrega entrega){
         return ResponseEntity.status(HttpStatus.CREATED)
@@ -34,26 +36,9 @@ public class EntregaController {
     public ResponseEntity<EntregaModel> buscar(@PathVariable("id") Long id){
         return entregaRepository.findById(id)
                 .map(entrega -> {
-                    EntregaModel entregaModel = new EntregaModel();
-                    entregaModel.setId(entrega.getId());
-                    entregaModel.setNomeCliente(entrega.getCliente().getNome());
-                    entregaModel.setDestinatario(new DestinatarioModel());
-                    entregaModel.getDestinatario().setNome(entrega.getDestinatario().getNome());
-                    entregaModel.getDestinatario().setLogradouro(entrega.getDestinatario().getLogradouro());
-                    entregaModel.getDestinatario().setNumero(entrega.getDestinatario().getNumero());
-                    entregaModel.getDestinatario().setComplemento(entrega.getDestinatario().getComplemento());
-                    entregaModel.getDestinatario().setBairro(entrega.getDestinatario().getBairro());
-                    entregaModel.setTaxa(entrega.getTaxa());
-                    entregaModel.setStatus(entrega.getStatus());
-                    entregaModel.setDataPedido(entrega.getDataPedido());
-                    entregaModel.setDataFinalizacao(entrega.getDataFinalizacao());
-
-
+                    EntregaModel entregaModel = modelMapper.map(entrega, EntregaModel.class);
                     return ResponseEntity.ok(entregaModel);
                 })
                 .orElse(ResponseEntity.notFound().build());
     }
-
-
-
 }
